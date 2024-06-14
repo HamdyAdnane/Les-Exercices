f99b6ddc084f65beafdfe88b60cbf0b52bdf88d7
Adnane Hamdy <a.hamdi@devaxy.com>
2024-06-14 11:02:00 +0100

    AjouterSodokuComplete

--- Les Exercices/AdnaneTools.cs
+++ Les Exercices/AdnaneTools.cs
@@ -1020,5 +1020,97 @@ namespace AdnaneTools
             }
             return i;
         }
-    }    
-}
+
+        static int N = 9;
+        public static bool solveSudoku(int[,] grid, int ligne, int colonne)
+        {
+            if (ligne == N - 1 && colonne == N)
+            {
+                return true;
+            }
+            if (colonne == N)
+            {
+                ligne++;
+                colonne = 0;
+            }
+            if (grid[ligne, colonne] != 0)
+            {
+                return solveSudoku(grid, ligne, colonne + 1);
+            }
+            for (int num = 1; num < 10; num++)
+            {
+                if (isSafe(grid, ligne, colonne, num))
+                {
+                    grid[ligne, colonne] = num;
+                    if (solveSudoku(grid, ligne, colonne + 1))
+                    {
+                        return true;
+                    }
+                }
+                grid[ligne, colonne] = 0;
+            }
+            return false;
+        }
+        public static void print(int[,] grid)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(grid[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool isSafe(int[,] grid, int ligne, int colonne, int num)
+        {
+            for (int x = 0; x <= 8; x++)
+            {
+                if (grid[ligne, x] == num)
+                {
+                    return false;
+                }
+            }
+            for (int x = 0; x <= 8; x++)
+            {
+                if (grid[x, colonne] == num)
+                {
+                    return false;
+                }
+            }
+            int startRow = ligne - ligne % 3, startCol = colonne - colonne % 3;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (grid[i + startRow, j + startCol] == num)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static void Principe()
+        {
+            int[,] grid = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+
+            if (solveSudoku(grid, 0, 0))
+            {
+                print(grid);
+            }
+            else
+            {
+                Console.WriteLine("No Solution exists");
+            }
+        }
+    }
+}    
